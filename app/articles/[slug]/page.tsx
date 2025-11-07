fb4ee973ebf9000a525ed844ab129a59617c10af
Jovan <jovanmedford@gmail.com>
2025-11-07 06:50:15 -0500

    Feat: update article page with container component

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -6,8 +6,10 @@ import {
 } from "@/lib/wordpress";
 import { FeaturedImage } from "./featured-image";
 import Author from "./author";
-import styles from "./article.module.css"
+import styles from "./article.module.css";
 import HorizontalSeparator from "@/app/components/horizontal-separator";
+import Container from "@/app/components/container";
+import Header from "@/app/components/header/header";
 
 export default async function Article({
   params,
@@ -32,15 +34,21 @@ export default async function Article({
   const img = getImageSrcFromPost(post);
   const author = getAuthorFromPost(post);
   return (
-    <div className="mx-6 max-w-3xl md:mx-auto">
-      <div className="flex flex-col gap-2">
-        <span className="font-bold block text-primary-500">{category}</span>
-        <h1 className="text-2xl font-serif font-bold">{post.title}</h1>
-        <Author author={author} />
-      </div>
-      <HorizontalSeparator />
-      <FeaturedImage img={img} />
-      <div className={styles.article} dangerouslySetInnerHTML={{ __html: post.content }} />
-    </div>
+    <section>
+      <Header />
+      <Container>
+        <div className="flex flex-col gap-2">
+          <span className="font-bold block text-primary-500">{category}</span>
+          <h1 className="text-2xl font-serif font-bold">{post.title}</h1>
+          <Author author={author} />
+        </div>
+        <HorizontalSeparator />
+        <FeaturedImage img={img} />
+        <div
+          className={styles.article}
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
+      </Container>
+    </section>
   );
 }
