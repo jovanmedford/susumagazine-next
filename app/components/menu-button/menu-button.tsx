250dca685c8bb51c6d9438bd5861e063dae55542
Jovan <jovanmedford@gmail.com>
2025-11-07 06:48:48 -0500

    Feat: add menu button, search button

---
+++ app/components/menu-button/menu-button.tsx
@@ -0,0 +1,6 @@
+import { Menu } from "lucide-react";
+import Button from "../button";
+
+export default function MenuButton() {
+  return <Button className="px-0" icon={Menu}>Menu</Button>;
+}
---
+++ app/components/search-button/search-button.tsx
@@ -0,0 +1,6 @@
+import { Search } from "lucide-react";
+import Button from "../button";
+
+export default function MenuButton() {
+  return <Button icon={Search}>Search</Button>;
+}
